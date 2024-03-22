d77f9b8f84b65210582d0cfb7b6852dabb164494
Sefa Demirtaş <39422788+tugsef@users.noreply.github.com>
2024-03-22 14:43:26 +0300

    update | codesandbox

--- app/[[...multiSelect]]/page.tsx
+++ app/[[...multiSelect]]/page.tsx
@@ -14,8 +14,6 @@ function page() {
       <Header />
       <MultiSelect />
       <Footer />
-      <h1 className="text-3xl">CodeSondex</h1>
-      <iframe src="https://codesandbox.io/p/github/tugsef/rick-and-morty/main" className="w-full h-full"></iframe>
     </div>
   );
 }
