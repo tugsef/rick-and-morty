4452f1c6afc5b0fef9d781f705f48893d8b91c2a
Sefa Demirtaş <39422788+tugsef@users.noreply.github.com>
2024-03-22 11:46:21 +0300

    update | ReadMe and Homepage update

--- app/[[...multiSelect]]/page.tsx
+++ app/[[...multiSelect]]/page.tsx
@@ -2,17 +2,21 @@ import React from "react";
 import Footer from "@/components/Footer";
 import MultiSelect from "@/components/MultiSelect";
 import Header from "@/components/Header";
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "Rick and Morty",
+  description: "Case",
+};
 function page() {
   return (
     <div className="h-screen w-full flex flex-col p-10 items-center gap-10">
-      <title>Rick and Morty</title>
       <Header />
       <MultiSelect />
       <Footer />
       <h1 className="text-xl font-extrabold">Github</h1>
       <iframe
-        src="https://github.com/tugsef/rick-and-morty"
+        src="https://tugsef.github.io/rick-and-morty"
         width="100%"
         className="h-full"
       ></iframe>
